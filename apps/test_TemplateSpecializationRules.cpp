c001326d950c39a9c847ec35f8b4d7cdbc43588e
Michael X. Grey <grey@openrobotics.org>
2018-12-13 19:53:46 +0800

    more template specialization tests

--- apps/test_TemplateSpecializationRules.cpp
+++ apps/test_TemplateSpecializationRules.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <vector>
 
 template<typename T>
 struct SomeClass
@@ -46,8 +47,22 @@ struct SomeClass<unsigned int>
   }
 };
 
+template<typename Element, typename Allocator>
+struct SomeClass<std::vector<Element, Allocator>>
+{
+  SomeClass()
+  {
+    std::cout << "std::vector<Element> template" << std::endl;
+  }
+};
+
+template<typename T>
+struct CustomAllocator : std::allocator<T> { };
 
 int main()
 {
   SomeClass<int> something;
+
+  SomeClass<std::vector<int>> vector;
+  SomeClass<std::vector<int, CustomAllocator<int>>> custom_allocator;
 }
